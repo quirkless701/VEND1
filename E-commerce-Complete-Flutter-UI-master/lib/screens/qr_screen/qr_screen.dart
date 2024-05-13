279c8c194f0ef196cad9c3a1ba3d4269842433ed
Boomer3110 <jaitrav@gmail.vom>
2024-05-14 04:46:55 +0530

    added sms reading

--- E-commerce-Complete-Flutter-UI-master/lib/screens/qr_screen/qr_screen.dart
+++ E-commerce-Complete-Flutter-UI-master/lib/screens/qr_screen/qr_screen.dart
@@ -1,10 +1,11 @@
 import 'dart:async';
-import 'dart:convert';
 import 'dart:typed_data';
+import '../../models/Cart.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:sms_receiver/sms_receiver.dart';
+import 'package:http/http.dart' as http;
+import 'package:permission_handler/permission_handler.dart';
+import 'package:readsms/readsms.dart';
 
 class PaymentScreen extends StatefulWidget {
   static const String routeName = "/payment";
@@ -12,7 +13,7 @@ class PaymentScreen extends StatefulWidget {
   const PaymentScreen({Key? key}) : super(key: key);
 
   @override
-  _PaymentScreenState createState() => _PaymentScreenState();
+  State<PaymentScreen> createState() => _PaymentScreenState();
 }
 
 class _PaymentScreenState extends State<PaymentScreen> {
@@ -20,8 +21,10 @@ class _PaymentScreenState extends State<PaymentScreen> {
   late Timer _timer;
   int _secondsRemaining = 360;
   double totalAmount = 0.0; // Total amount variable
-  String? _smsContent = 'Waiting for SMS...'; // SMS content variable
-  SmsReceiver? _smsReceiver;
+  final _plugin = Readsms();
+  String sms = 'no sms received';
+  String sender = 'no sms received';
+  String time = 'no sms received';
 
   @override
   void initState() {
@@ -29,15 +32,51 @@ class _PaymentScreenState extends State<PaymentScreen> {
     calculateTotal(); // Call calculateTotal function
     fetchQRImage(totalAmount); // Pass the amount
     startTimer();
-    _smsReceiver = SmsReceiver(onSmsReceived);
-    _startListeningForSMS();
+    getPermission().then((value) {
+      if (value) {
+        _plugin.read();
+        _plugin.smsStream.listen((event) {
+          setState(() {
+            sms = event.body;
+            sender = event.sender;
+            time = event.timeReceived.toString();
+
+            print('Received SMS: $sms');
+            print('From: $sender');
+            print('Received at: $time');
+
+            String orderTotalPattern = 'Rs\\.${totalAmount.toStringAsFixed(2)}';
+
+            bool isCredited = RegExp(r'credited').hasMatch(sms);
+            bool hasExactOrderTotal = RegExp(orderTotalPattern).hasMatch(sms);
+
+            if (isCredited && hasExactOrderTotal) {
+              print('SMS is valid');
+              // Now you can proceed with further actions
+            }
+          });
+        });
+      }
+    });
+  }
+
+  Future<bool> getPermission() async {
+    if (await Permission.sms.status == PermissionStatus.granted) {
+      return true;
+    } else {
+      if (await Permission.sms.request() == PermissionStatus.granted) {
+        return true;
+      } else {
+        return false;
+      }
+    }
   }
 
   @override
   void dispose() {
     _timer.cancel();
-    _smsReceiver?.stopListening();
     super.dispose();
+    _plugin.dispose();
   }
 
   void fetchQRImage(double amount) async {
@@ -61,7 +100,7 @@ class _PaymentScreenState extends State<PaymentScreen> {
   }
 
   void startTimer() {
-    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
       setState(() {
         if (_secondsRemaining > 0) {
           _secondsRemaining--;
@@ -74,23 +113,30 @@ class _PaymentScreenState extends State<PaymentScreen> {
   }
 
   void _cancelPayment() {
+    _timer.cancel(); // Cancel the timer
+
     showDialog(
       context: context,
+      barrierDismissible: true, // Allow tapping outside to dismiss
       builder: (BuildContext context) {
-        return GestureDetector(
-          onTap: () {
-            Navigator.of(context).pop(); // Close the dialog when tapping outside
+        return WillPopScope(
+          onWillPop: () async {
+            _timer.cancel(); // Cancel the timer
+            Navigator.of(context).pop();
+            Navigator.of(context).pop(); // Go back to the cart screen
+            return false; // Prevents default back navigation
           },
           child: AlertDialog(
-            title: Text("Payment Cancelled"),
-            content: Text("Your payment was cancelled."),
+            title: const Text("Payment Cancelled"),
+            content: const Text("Your payment was cancelled."),
             actions: [
               TextButton(
                 onPressed: () {
+                  _timer.cancel(); // Cancel the timer
                   Navigator.of(context).pop(); // Close the alert dialog
-                  Navigator.of(context).pop(); // Go back to the previous screen
+                  Navigator.of(context).pop(); // Go back to the cart screen
                 },
-                child: Text("OK"),
+                child: const Text("OK"),
               ),
             ],
           ),
@@ -99,58 +145,8 @@ class _PaymentScreenState extends State<PaymentScreen> {
     );
   }
 
-  void _startListeningForSMS() async {
-    if (_smsReceiver == null) return;
-    await _smsReceiver?.startListening();
-    setState(() {
-      _smsContent = 'Waiting for SMS...';
-    });
-  }
 
-  void onSmsReceived(String? message) {
-    if (message != null) {
-      setState(() {
-        _smsContent = message;
-        if (_smsContent!.contains('Rs.') &&
-            _smsContent!.contains('credited') &&
-            _smsContent!.contains('linked to VPA')) {
-          // Extract amount from the SMS and compare with totalAmount
-          // If amount matches, process the payment
-          _processPayment();
-        }
-      });
-    }
-  }
 
-  void _processPayment() {
-    // Your code to process the payment after reading the SMS
-    // For example, send a signal to the ESP32 to turn the servo
-    // This could be done using HTTP requests or other communication methods
-
-    // Example of sending an HTTP request to the ESP32 with JSON payload
-    String esp32Url = 'http://192.168.137.190/turn_servo'; // Replace with your ESP32 IP address and endpoint
-    Map<String, dynamic> payload = {
-      'quantity1': 3, // Example quantity for servo 1
-      'quantity2': 2, // Example quantity for servo 2
-    };
-    http.post(Uri.parse(esp32Url), body: jsonEncode(payload)).then((response) {
-      if (response.statusCode == 200) {
-        // Successful response from ESP32
-        print('Signal sent to ESP32 to turn servo');
-      } else {
-        // Handle error response from ESP32
-        print('Error sending signal to ESP32');
-      }
-    }).catchError((error) {
-      // Handle network error
-      print('Network error: $error');
-    });
-  }
-
-  void calculateTotal() {
-    // Your code to calculate the total amount
-    // Assume the total amount calculation is already implemented
-  }
 
   String formatTimer(int seconds) {
     final minutes = seconds ~/ 60;
@@ -158,11 +154,22 @@ class _PaymentScreenState extends State<PaymentScreen> {
     return '$minutes:${remainingSeconds.toString().padLeft(2, '0')}';
   }
 
+  // Function to calculate the total amount
+  void calculateTotal() {
+    double total = 0.0;
+    for (var cartItem in demoCarts) {
+      total += cartItem.product.price * cartItem.numOfItem;
+    }
+    setState(() {
+      totalAmount = total;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Payment"),
+        title: const Text("Payment"),
       ),
       body: SafeArea(
         child: SingleChildScrollView(
@@ -170,7 +177,7 @@ class _PaymentScreenState extends State<PaymentScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              SizedBox(
+              const SizedBox(
                 width: double.infinity,
                 child: Text(
                   "Complete Your Payment",
@@ -188,23 +195,18 @@ class _PaymentScreenState extends State<PaymentScreen> {
                   fit: BoxFit.contain,
                 ),
               )
-                  : CircularProgressIndicator(), // Loading indicator while waiting for response
+                  : const CircularProgressIndicator(), // Loading indicator while waiting for response
               const SizedBox(height: 16),
               Text(
                 'Time remaining: ${formatTimer(_secondsRemaining)}',
-                style: TextStyle(fontSize: 20),
-              ),
-              const SizedBox(height: 16),
-              Text(
-                'SMS Content: $_smsContent',
-                style: TextStyle(fontSize: 18),
+                style: const TextStyle(fontSize: 20),
               ),
               const SizedBox(height: 16),
               ElevatedButton(
                 onPressed: () {
                   _cancelPayment(); // Cancel payment when cancel button is pressed
                 },
-                child: Text("Cancel Payment"),
+                child: const Text("Cancel Payment"),
               ),
             ],
           ),
